ef46883b765a99a20d327d3afc1d4e5622a9d469
FlorianGelb <florian.braun02@gmail.com>
2022-11-09 17:28:03 +0100

    problem solved

--- IBVP.m
+++ IBVP.m
@@ -1,8 +1,9 @@
+clear;clc;close all
 tic
-N = 25;
+N = 50;
 dt = 0.0001
 X = 1;
-T = 2;
+T = 1;
 K = zeros(N);
 M = zeros(N);
 delta_x = X / (N-1);
@@ -12,12 +13,12 @@ t(x) = triangularPulse(0, delta_x, 2 * delta_x, x)^2;
 t2(x)= triangularPulse(0, delta_x, 2 * delta_x, x) * triangularPulse(delta_x, 2 * delta_x, 3* delta_x, x);
 ii = integral(matlabFunction(t(x)), 0, X);
 ij = integral(matlabFunction(t2(x)), 0, X);
-f(x) = 1;%exp(-0.5*(x-5)) + exp(0.5*(x-X+15));
+f(x) = 100*exp(-0.5*(x-5)) + exp(0.5*(x-X+15));
 F = zeros(N, 1);
 p(x) = 0
 H(1) = f(0);
 H(end) = f(X);
-alpha = 0.001;
+alpha = 0.0005;
 
 for i = 1:N-2
   F(i+1) =   integral(matlabFunction(f(x) * triangularPulse((i-1)*delta_x, (i)*delta_x, (i+1)*delta_x, x)), 0, X);
@@ -58,7 +59,7 @@ for i=dt:dt:T
 end
 
 
-dx = 0.01;
+dx = 0.001;
 S = []
 for t = 1:1:T/dt
 c = C(:, t);
@@ -68,7 +69,7 @@ end
 
 [U, Z, V] = svd(S,"econ");
 energies = diag(Z) / trace(Z);
-e_t = 0.95;
+e_t = 0.90;
 e = 0;
 trunc = 0;
 for i = 1:size(energies, 1)
@@ -82,8 +83,13 @@ end
 U2 = U(:, 1:trunc);
 
 phi = [];
+%for i = 1:trunc
+%phi(:, i) = Nderiv2(Nderiv2(U2(:, i), dx), dx);
+%end
+
+k = (2*pi/X) * [0:X/dx/2-1 -X/dx/2:-1].';
 for i = 1:trunc
-phi(:, i) = Nderiv2(Nderiv2(U2(:, i), dx), dx);
+    phi(:,i) = ifft(-(k.^2).*fft(U2(:, i)));
 end
 
 a = U2.'*S(:, 1);
@@ -91,21 +97,24 @@ a0 = a;
 A = []
 A(1, :) = U2*a;
 j = 2;
+Y = U2.'*phi;
 
-%dt = dt/10;
-%{for i=dt:dt:T
-    %a_n = dt*alpha * U2.'*phi*a + a;
-    %a = a_n;
-    %A(j, :) = U2*a;
-    %j= j +1;
-   %end
-   
-%A = A.';
+%for i=dt:dt:T
+%    a_n = dt * alpha*Y*a + a;
+ %   a = a_n;
+ %   A(j, :) = U2*a;
+  %  j= j +1;
+%end
+A = A.';
+
 [t, asol] = ode45("a_rhs", [0:dt:T], a0,[], U2, phi, alpha);
+usol = zeros(size(U2, 1), length(t)).';
 for j = 1:length(t)
-    usol(j,:) = asol(j,1)*U2(:,1) + asol(j,2)*U2(:,2);
+    for i = 1:trunc;
+        usol(j,:) = usol(j, :) + asol(j,i)*U2(:,i).';
+    end
 end
-A = usol;
+A = usol.';
 figure(1)
 imagesc(C)
 colormap turbo
