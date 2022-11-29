1aa15507e3305b3dccef0cc5b0afee7e7ba1dfda
Florian Gelb <irolori64@gmail.com>
2022-11-29 14:43:48 +0100

    pull

--- OOP/model/containerFEM.m
+++ OOP/model/containerFEM.m
@@ -1,6 +1,4 @@
 classdef containerFEM < container
-
-    
     properties
        
     end
--- OOP/test.m
+++ OOP/test.m
@@ -7,7 +7,7 @@ k = (2*pi/L) * [0:n/2-1 -n/2:-1].';
 X = linspace(0, L, n);
 t = linspace(0, T, nt);
 alpha = 1
-u0 = sin(2*pi/L * X);
+u0 = 0*sin(2*pi/L * X) + 1;
 
 m = main(["FT"], L, u0, T, n, nt, alpha);
 m = m.solve();
